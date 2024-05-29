0000074666e74ca851e2d68e42c9c9b4372cbdb3
Miguel Martinazzo <miguelmartinazzo@gmail.com>
2024-05-29 00:48:13 -0300

    fix: fixed the route to /forum when submit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
-
 import { SignupComponent } from './pages/signup/signup.component';
 import { AdminComponent } from './pages/admin/admin.component';
 import { authGuard } from './auth/auth.guard';
+import { ForumComponent } from './pages/forum/forum.component';
 
 const routes: Routes = [
   //Variables that contains all functions for login page
@@ -20,6 +20,10 @@ const routes: Routes = [
 
   {
     path: 'admin', component: AdminComponent, canActivate: [authGuard]
+  },
+
+  {
+    path: 'forum', component: ForumComponent, canActivate: [authGuard]
   }
 ];
 
